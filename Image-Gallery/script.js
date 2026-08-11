dba3ed4492560a31b64654350fb4142acbc76b70
Minahil Amjad <minahilmirza9876@gmail.com>
2026-08-11 16:09:57 +0500

    Image-Gallery

--- Image-Gallery/script.js
+++ Image-Gallery/script.js
@@ -2,4 +2,12 @@ const galleryImags=document.querySelectorAll('.gallery-container img');
 const box=document.getElementById('#box');
 const boxImg=document.getElementById('box-img');
 
+let currentImgindex=0;
+ 
+// functions calling
 
+function openbox(index){
+    currentImgindex=index;
+    boxImg.src=galleryImages[currentImgindex].src;
+    box.style.display='flex';
+}
